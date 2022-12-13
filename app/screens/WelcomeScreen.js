01df59e5ff909943ddb2e7fea39261d96876cf8c
shaanarora19 <arorashaan7@gmail.com>
2022-12-12 21:48:39 -0500

    more tings

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 function WelcomeScreen(props) {
     return (
-        <div>
-            
-        </div>
+        <Image style={styles.mainImage} source={require("../assets/welcomescreenpic.png")}> </Image>
     );
 }
+
+const styles = StyleSheet.create({
+    mainImage: {
+        width: 100,
+        height: 100,
+        position: 'absolute',
+        top: 50,
+    }
+})
 
 export default WelcomeScreen;
